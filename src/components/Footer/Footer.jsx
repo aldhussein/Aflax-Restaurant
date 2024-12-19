07879593674f5c8458420d91138c8e95ddd63c72
aldhussein <145017137+aldhussein@users.noreply.github.com>
2024-12-19 17:00:02 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <div className=" grid md:grid-cols-3 py-5">
           <div className=" py-8 px-4 ">
             <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
-              <img src={footerLogo} alt="Logo" className="max-w-[50px]" />
+              <img src={footerLogo} alt="Logo" className="max-w-[50px] text-black" />
               AFLAX
             </h1>
             <p className="">
